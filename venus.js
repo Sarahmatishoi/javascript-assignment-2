81161fcdb6f1272413ab3b4b542fa753a73d129e
sarahmatishoi <sarahmatishoi@gmail.com>
2021-03-21 15:40:09 +0300

    second commit

---
+++ venus.js
@@ -0,0 +1,16 @@
+var fruit={
+    name:"oranges",
+    price:30.00,
+    quantity:2,
+    fruitcost:function(price,quantity){
+        totalPrice=price*quantity
+        return this .fruitcost=totalPrice
+
+    }
+
+
+
+}
+fruit.fruitcost(30.00,2);
+console.log ( fruit.quantity + " " + fruit.name + " "  + " for  KES " + totalPrice );
+
